f46b838d237782220b984ef57666a6ba0f9d3ae9
Cathleen Wright <cathleenmwright@gmail.com>
2018-03-16 20:32:16 -0400

    2.6 Create Register Form

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+//Connection Variable
+$con = mysqli_connect("localhost", "root", "", "social");
+
+//If error connecting to DB:
+if(mysqli_connect_errno()) {
+  echo "Failed to connect: " . mysqli_connect_errno();
+}
+
+ ?>
+
+
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Social</title>
+  </head>
+  <body>
+
+  </body>
+</html>
---
+++ register.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Social</title>
+  </head>
+  <body>
+    <form action="register.php" method="POST">
+      <input type="text" name="reg_fname" placeholder="First Name" required>
+      <br>
+      <input type="text" name="reg_lname" placeholder="Last Name" required>
+      <br>
+      <input type="email" name="reg_email" placeholder="Email" required>
+      <br>
+      <input type="email" name="reg_email2" placeholder="Confirm Email" required>
+      <br>
+      <input type="password" name="reg_password" placeholder="Password" required>
+      <br>
+      <input type="password" name="reg_password2" placeholder="Confirm Password" required>
+      <br>
+      <input type="submit" name="register_button" value="Register">
+    </form>
+  </body>
+</html>
