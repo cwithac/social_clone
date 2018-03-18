cac5ca4416604330d692bc73b50dff8eb8d46b53
Cathleen Wright <cathleenmwright@gmail.com>
2018-03-17 23:10:49 -0400

    6.39 User Details add and styling, index

--- index.php
+++ index.php
@@ -3,7 +3,16 @@ include('includes/header.php');
  ?>
 
  <div class="user_details column">
-  <a href="#"><img src="<?php echo $user['profile_pic']; ?> " alt="profile_pic"> </a>
+   <a href="<?php echo $userLoggedIn; ?>">  <img src="<?php echo $user['profile_pic']; ?>"> </a>
+   <div class="user_details_left_right">
+     <a href="<?php echo $userLoggedIn; ?>">
+     <?php echo $user['first_name'] . " " . $user['last_name'];?>
+     </a>
+     <br>
+     <?php echo "Posts: " . $user['num_posts']. "<br>";
+     echo "Likes: " . $user['num_likes']; ?>
+   </div>
+
  </div>
  <!-- WRAPPER BELOW CLOSE FROM header.php -->
   </div>
