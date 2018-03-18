f9425253a47a9a5f6c7711f0c6b45534f5b3c3d0
Cathleen Wright <cathleenmwright@gmail.com>
2018-03-18 12:51:38 -0400

    7.48, 7.49, 7.50 Loading Posts Part 1, Part 2 (Closed Account), Part 3 (Timestamp)

--- includes/classes/Post.php
+++ includes/classes/Post.php
@@ -36,4 +36,83 @@ class Post {
   }
 }
 
+  public function loadPostsFriends() {
+    $str = ''; //String to return, initialize
+    $data = mysqli_query($this->con, "SELECT * FROM posts WHERE deleted='no' ORDER BY id DESC");
+    while($row = mysqli_fetch_array($data)) {
+      $id = $row['id'];
+      $body = $row['body'];
+      $added_by = $row['added_by'];
+      $time = $row['date_added'];
+
+      //Prepare user_to string to be included if necessary
+      if($row['user_to'] == 'none') {
+        $user_to  = '';
+      } else {
+        $user_to_obj = new User($con, $row['user_to']); //New instance of user class
+        $user_to_name = $user_to_obj->getFirstAndLastName(); //Gets first and last name of user
+        $user_to = "<a href='" . $row['user_to'] . "'>" . $user_to_name . "</a>"; //Return link to the profile page
+      }
+
+      //Check if user who posted has a closed accounts (posts will not show)
+      $added_by_obj = new User($con, $added_by);
+      if($added_by_obj->isClosed()) {
+        continue; //Returns to start of while loop
+      }
+      //Else
+      $user_details_query = mysqli_query($this->con, "SELECT first_name, last_name, profile_pic FROM users WHERE username='$added_by'");
+      $user_row = mysqli_fetch_array($user_details_query);
+            //Timeframe
+            $date_time_now = date("Y-m-d H:i:s");
+            $start_date = new DateTime($date_time); //Time of post
+            $end_date = new DateTime($date_time_now); //Current time
+            $interval = $start_date->diff($end_date); //Difference between dates
+            if($interval->y >= 1) {
+                if($interval == 1) {
+                  $time_message = $interval->y . " year ago"; //1 year ago
+                } else {
+                  $time_message = $interval->y . " years ago"; //1+ year ago
+                };
+            } else if ($interval-> m >= 1) {
+                if($interval->d == 0) {
+                  $days = " ago";
+                } else if($interval->d == 1) {
+                  $days = $interval->d . " day ago";
+                } else {
+                  $days = $interval->d . " days ago";
+                };
+                if($interval->m == 1) {
+                  $time_message = $interval->m . " month". $days;
+                } else {
+                  $time_message = $interval->m . " months". $days;
+                };
+            } else if($interval->d >= 1) {
+                if($interval->d == 1) {
+                  $time_message = "Yesterday";
+                } else {
+                  $time_message = $interval->d . " days ago";
+                }
+              } else if($interval->h >= 1) {
+                if($interval->h == 1) {
+                  $time_message = $interval->h . " hour ago";
+                } else {
+                  $time_message = $interval->h . " hours ago";
+                }
+              } else if($interval->i >= 1) {
+                if($interval->i == 1) {
+                  $time_message = $interval->i . " minute ago";
+                } else {
+                  $time_message = $interval->i . " minutes ago";
+                }
+            } else {
+                if($interval->s < 30) {
+                  $time_message = "Just now";
+                } else {
+                  $time_message = $interval->s . " seconds ago";
+                }
+            };
+
+      } //End while loop
+  }
+
  ?>
--- includes/classes/User.php
+++ includes/classes/User.php
@@ -27,6 +27,17 @@ class User {
     $row = mysqli_fetch_array($query);
     return $row['num_posts'];
   }
+
+  public function isClosed() {
+    $username = $this->user['username'];
+    $query = mysqli_query($this->con, "SELECT user_closed FROM users WHERE username='$username'");
+    $row = mysqli_fetch_array($query);
+    if($row['user_closed'] == 'yes') {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
 
  ?>
