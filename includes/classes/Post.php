4cdc6a740e05abeb6fd50d41a1d7a2e0f83c0c83
Cathleen Wright <cathleenmwright@gmail.com>
2018-03-18 13:21:26 -0400

    7.52 Styling the posts in newsfeed, index

--- includes/classes/Post.php
+++ includes/classes/Post.php
@@ -120,8 +120,8 @@ class Post {
                   <div class='post_profile_pic'>
                     <img src='$profile_pic' width='50'>
                   </div>
-                  <div class='posted_by' style='color:#ACACAC;'>
-                    <a href='$added_by'> $first_name $last_name </a> $user_to &nbsp;&nbsp;&nbsp;&nbsp;$time_message
+                  <div class='posted_by' style='color:#bdc3c7;'>
+                    <a href='$added_by'> $first_name $last_name </a> $user_to &nbsp;&nbsp;&nbsp;&nbsp;<span class='time_message'>$time_message</span>
                   </div>
                   <div id='post_body'>
                     $body
