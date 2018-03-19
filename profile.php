571589c0d29ddc36140600ff194c2b61c8368360
Cathleen Wright <cathleenmwright@gmail.com>
2018-03-19 19:50:21 -0400

    9.74 Left Profile Nav content

--- profile.php
+++ profile.php
@@ -1,10 +1,27 @@
 <?php
 include('includes/header.php');
+
+if(isset($_GET['profile_username'])) {
+  //Username from .htaccess URL parameter
+  $username = $_GET['profile_username'];
+  $user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$username'");
+  $user_array = mysqli_fetch_array($user_details_query); //Array results of user info
+
+    $num_friends = (substr_count($user_array['friend_array'], ',') - 1); //Splice friends array, excluding initial ',' for 0
+}
  ?>
 
+ <div class="profile_left">
+   <img src="<?php echo $user_array['profile_pic']; ?> " alt="user_profile_pic">
+   <div class="profile_info">
+     <p><?php echo "Posts: " . $user_array['num_posts'];?></p>
+     <p><?php echo "Likes: " . $user_array['num_likes'];?></p>
+     <p><?php echo "Friends: " . $num_friends;?></p>
+   </div>
+ </div>
 
  <div class="main_column column">
-   PROFILE
+   <?php echo $username ?>
  </div>
  <!-- WRAPPER BELOW CLOSE FROM header.php -->
   </div>
