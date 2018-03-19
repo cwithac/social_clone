2f09aa4d0de13421ac646683de5769c062d10e12
Cathleen Wright <cathleenmwright@gmail.com>
2018-03-19 10:31:16 -0400

    8.67 Display number of comments in newsfeed

--- includes/classes/Post.php
+++ includes/classes/Post.php
@@ -109,6 +109,10 @@ class Post {
               </script>
 
           <?php
+
+          $comments_check = mysqli_query($this->con, "SELECT * FROM comments WHERE post_id='$id'");
+          $comments_check_num = mysqli_num_rows($comments_check); //Number of results of comments per post
+
                 //Timeframe
                 $date_time_now = date("Y-m-d H:i:s");
                 $start_date = new DateTime($date_time); //Time of post
@@ -171,6 +175,12 @@ class Post {
                       <div id='post_body'>
                         $body
                         <br>
+                        <br>
+                        <br>
+                      </div>
+                      <div class='newsfeedPostOptions'>
+                        Comments&nbsp;($comments_check_num)&nbsp;&nbsp;&nbsp;
+
                       </div>
                     </div>
                     <div class='post_comment' id='toggleComment$id' style='display: none;'>
