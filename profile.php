853848176b3d490d545fadfb7c088050c3d5628c
Cathleen Wright <cathleenmwright@gmail.com>
2018-03-20 12:10:21 -0400

    9.84 Non-functioning modal on profile for peer to peer posting using bootstrap

--- profile.php
+++ profile.php
@@ -67,12 +67,44 @@ if(isset($_GET['profile_username'])) {
         }
       }
        ?>
+
     </form>
+    <input type="submit" class="default" data-toggle="modal" data-target="#post_form" value="Say something...">
  </div>
 
  <div class="main_column column">
    <?php echo $username ?>
  </div>
+
+ <!-- Modal -->
+ <div class="modal fade" id="post_form" tabindex="-1" role="dialog" aria-labelledby="postModalLabel">
+   <div class="modal-dialog" role="document">
+     <div class="modal-content">
+
+       <div class="modal-header">
+         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+         <h4 class="modal-title" id="myModalLabel">Say something...</h4>
+       </div>
+
+       <div class="modal-body">
+         <p>Post to another user's profile page and their newsfeed for your friends to see!</p>
+         <form class="profile_post" action="" method="POST">
+           <div class="form-group">
+             <textarea class="form-control" name="post_body"></textarea>
+             <input type="hidden" name="user_from" value="<?php echo $userLoggedIn ?>">
+             <input type="hidden" name="user_to" value="<?php echo $username ?>">
+           </div>
+         </form>
+       </div>
+
+       <div class="modal-footer">
+         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+         <button type="button" class="btn btn-primary">Post</button>
+       </div>
+     </div>
+   </div>
+ </div>
+
  <!-- WRAPPER BELOW CLOSE FROM header.php -->
   </div>
   </body>
