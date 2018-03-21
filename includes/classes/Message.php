234358d391ca6212c33870d6402633c21d3db172
Cathleen Wright <cathleenmwright@gmail.com>
2018-03-21 17:40:14 -0400

    10.103 Styling messages, sending error cleanup

--- includes/classes/Message.php
+++ includes/classes/Message.php
@@ -1,63 +1,62 @@
 <?php
-
 class Message {
-  private $user;
-  private $con;
+	private $user_obj;
+	private $con;
 
-  public function __construct($con, $user) {
-    $this->con = $con;
-    $this->user_obj = new User($con, $user);
-  }
+	public function __construct($con, $user){
+		$this->con = $con;
+		$this->user_obj = new User($con, $user);
+	}
 
-  public function getMostRecentUser() {
-    $userLoggedIn = $this->user_obj->getUsername();
+	public function getMostRecentUser() {
+		$userLoggedIn = $this->user_obj->getUsername();
 
-    $query = mysqli_query($this->con, "SELECT user_to, user_from FROM messages WHERE user_to='$userLoggedIn' OR user_from='$userLoggedIn' ORDER BY id DESC LIMIT 1"); //Single most recent result, message to or from logged in user
+		$query = mysqli_query($this->con, "SELECT user_to, user_from FROM messages WHERE user_to='$userLoggedIn' OR user_from='$userLoggedIn' ORDER BY id DESC LIMIT 1");
 
-    if(mysqli_num_rows($query) == 0) {
-      return false;
-    }
+		if(mysqli_num_rows($query) == 0)
+			return false;
 
-    $row = mysqli_fetch_array($query);
-    $user_to = $row['user_to'];
-    $user_from = $row['user_from'];
+		$row = mysqli_fetch_array($query);
+		$user_to = $row['user_to'];
+		$user_from = $row['user_from'];
 
-    if($user_to != $userLoggedIn) {
-      return $user_to;
-    } else {
-      return $user_from;
-    }
+		if($user_to != $userLoggedIn)
+			return $user_to;
+		else
+			return $user_from;
 
-  }
+	}
 
-  public function sendMessage($user_to, $body, $date) {
-    if($body != '') {
-      //Body is not empty
-      $userLoggedIn = $this->user_obj->getUsername();
+	public function sendMessage($user_to, $body, $date) {
+
+		if($body != "") {
+			$userLoggedIn = $this->user_obj->getUsername();
       //Messages Table: id, user_to, user_from, body, date, opened, viewed, deleted
-      $query = mysqli_query($this->con, "INSERT INTO messages VALUES('', '$user_to', '$userLoggedIn', '$body', '$date', 'no', 'no', 'no')");
-    }
-  }
+			$query = mysqli_query($this->con, "INSERT INTO messages VALUES('', '$user_to', '$userLoggedIn', '$body', '$date', 'no', 'no', 'no')");
+		}
+	}
 
-  public function getMessages($otherUser) {
-    $userLoggedIn = $this->user_obj->getUsername();
-    $data = ""; //String to be returned
-    $query = mysqli_query($this->con, "UPDATE messages SET opened='yes' WHERE user_to='$userLoggedIn' AND user_from='$otherUser'"); //All opened messages
+	public function getMessages($otherUser) {
+		$userLoggedIn = $this->user_obj->getUsername();
+		$data = "";
 
-    	$get_messages_query = mysqli_query($this->con, "SELECT * FROM messages WHERE (user_to='$userLoggedIn' AND user_from='$otherUser') OR (user_from='$userLoggedIn' AND user_to='$otherUser')"); //Both sides of messages
+		$query = mysqli_query($this->con, "UPDATE messages SET opened='yes' WHERE user_to='$userLoggedIn' AND user_from='$otherUser'");
 
-      while($row = mysqli_fetch_array($get_messages_query)) {
-  			$user_to = $row['user_to'];
-  			$user_from = $row['user_from'];
-  			$body = $row['body'];
+		$get_messages_query = mysqli_query($this->con, "SELECT * FROM messages WHERE (user_to='$userLoggedIn' AND user_from='$otherUser') OR (user_from='$userLoggedIn' AND user_to='$otherUser')");
 
-  			$div_top = ($user_to == $userLoggedIn) ? "<div class='message' id='sent'>" : "<div class='message' id='received'>";
-  			$data = $data . $div_top . $body . "</div><br><br>";
-  		}
-  		return $data;
-  	}
+		while($row = mysqli_fetch_array($get_messages_query)) {
+			$user_to = $row['user_to'];
+			$user_from = $row['user_from'];
+			$body = $row['body'];
+
+			$div_top = ($user_to == $userLoggedIn) ? "<div class='message' id='received'>" : "<div class='message' id='sent'>";
+			$data = $data . $div_top . $body . "</div><br><br>";
+		}
+		return $data;
+	}
 
 }// End  Class
+
 
 
 
--- messages.php
+++ messages.php
@@ -1,68 +1,83 @@
 <?php
-include('includes/header.php');
+include("includes/header.php");
 
 $message_obj = new Message($con, $userLoggedIn);
 
-if(isset($_GET['u'])) {
-  $user_to = ($_GET['u']); //Set in the URL params
-} else {
-  $user_to = $message_obj->getMostRecentUser(); //Most recent user messaged
-  if($user_to == false) {
-    $user_to = 'new'; //No conversations yet, start a new
-  }
+if(isset($_GET['u']))
+	$user_to = $_GET['u'];
+else {
+	$user_to = $message_obj->getMostRecentUser();
+	if($user_to == false)
+		$user_to = 'new';
 }
 
-if($user_to != 'new') {
-  $user_to_obj = new User($con, $user_to);
-}
+if($user_to != "new")
+	$user_to_obj = new User($con, $user_to);
 
 if(isset($_POST['post_message'])) {
-  //Message send pressed
-  if(isset($_POST['message_body'])) {
-    //There is text in the message
-    $body = mysqli_real_escape_string($con, $_POST['message_body']); //Prepares string for SQL
-    $date = date("Y-m-d H:i:s");
-    $message_obj->sendMessage($user_to, $body, $date);
-  }
+
+	if(isset($_POST['message_body'])) {
+		$body = mysqli_real_escape_string($con, $_POST['message_body']);
+		$date = date("Y-m-d H:i:s");
+		$message_obj->sendMessage($user_to, $body, $date);
+	}
+
 }
 
-?>
+ ?>
 
-<div class="user_details column">
-  <a href="<?php echo $userLoggedIn; ?>">  <img src="<?php echo $user['profile_pic']; ?>"> </a>
-  <div class="user_details_left_right">
-    <a href="<?php echo $userLoggedIn; ?>">
-    <?php echo $user['first_name'] . " " . $user['last_name'];?>
-    </a>
-    <br>
-    <?php echo "Posts: " . $user['num_posts']. "<br>";
-    echo "Likes: " . $user['num_likes']; ?>
-  </div>
-</div>
+ <div class="user_details column">
+		<a href="<?php echo $userLoggedIn; ?>">  <img src="<?php echo $user['profile_pic']; ?>"> </a>
 
-<div class="main_column column" id="main_column">
-  <?php
-    if($user_to != 'new') {
-      echo "<h4>You and <a href='$user_to'>" . $user_to_obj->getFirstAndLastName() . "</a></h4><hr><br>";
-      echo "<div class='loaded_messages'>";
-        echo $message_obj->getMessages($user_to);
-      echo "</div>";
-    } else {
-      echo "<h4>New Message</h4>";
-    }
-   ?>
-  <div class="message_post">
-    <form action="" method="POST">
-      <?php
-        if($user_to == 'new') {
-          echo 'Select the friend you would like to message! <br><br>';
-          echo "To: <input type='text'>";
-          echo "<div class='results'></div>";
-        }  else {
-          echo "<textarea name='message_body' id='message_textarea' placeholder='Write something ...'></textarea>";
-          echo "<input type='submit' name='post_message' class='info' id='message_submit' value='Send'>";
-        }
-       ?>
-    </form>
-  </div>
+		<div class="user_details_left_right">
+			<a href="<?php echo $userLoggedIn; ?>">
+			<?php
+			echo $user['first_name'] . " " . $user['last_name'];
+
+			 ?>
+			</a>
+			<br>
+			<?php echo "Posts: " . $user['num_posts']. "<br>";
+			echo "Likes: " . $user['num_likes'];
+
+			?>
+		</div>
+	</div>
+
+	<div class="main_column column" id="main_column">
+		<?php
+		if($user_to != "new"){
+			echo "<h4>You and <a href='$user_to'>" . $user_to_obj->getFirstAndLastName() . "</a></h4><hr><br>";
+
+			echo "<div class='loaded_messages' id='scroll_messages'>";
+				echo $message_obj->getMessages($user_to);
+			echo "</div>";
+		}
+		else {
+			echo "<h4>New Message</h4>";
+		}
+		?>
+
+
+
+		<div class="message_post">
+			<form action="" method="POST">
+				<?php
+				if($user_to == "new") {
+					echo "Select the friend you would like to message <br><br>";
+					?>
+					To: <input type='text' onkeyup='getUsers(this.value, "<?php echo $userLoggedIn; ?>")' name='q' placeholder='Name' autocomplete='off' id='seach_text_input'>
+
+					<?php
+					echo "<div class='results'></div>";
+				}
+				else {
+					echo "<textarea name='message_body' id='message_textarea' placeholder='Write something ...'></textarea>";
+					echo "<input type='submit' name='post_message' class='info' id='message_submit' value='Send'>";
+				}
+
+				?>
+			</form>
+
+		</div>
 </div>
