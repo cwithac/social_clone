12c69201793d05070cc4586bb5e8c7fe341abf48
Cathleen Wright <cathleenmwright@gmail.com>
2018-03-22 11:09:58 -0400

    11.119 Prep retrieve data from dropdown messages

--- includes/classes/Message.php
+++ includes/classes/Message.php
@@ -149,6 +149,63 @@ class Message {
     return $return_string;
   }
 
+	public function getConvosDropdown($data, $limit) {
+		$page = $data['page'];
+		$userLoggedIn = $this->user_obj->getUsername();
+		$return_string = "";
+		$convos = array();
+
+			if($page == 1) {
+				$start = 0;
+			} else {
+				$start = ($page - 1) + $limit;
+			}
+
+		$set_viewed_query = mysqli_query($this->con, "UPDATE messages SET viewed='yes' WHERE user_to='$userLoggedIn'");
+
+		$query = mysqli_query($this->con, "SELECT user_to, user_from FROM messages WHERE user_to='$userLoggedIn' OR user_from='$userLoggedIn' ORDER by id DESC");
+
+		while($row = mysqli_fetch_array($query)) {
+			$user_to_push = ($row['user_to'] != $userLoggedIn) ? $row['user_to'] : $row['user_from'];
+			if(!in_array($user_to_push, $convos)) {
+				array_push($convos, $user_to_push);
+			}
+		}
+
+		$num_iterations = 0; //Number of messages checked
+		$count = 1; //Number of messages posted
+
+
+		foreach($convos as $username) {
+				if($num_iterations++ < $start) {
+					continue;
+				}
+				if($count > $limit) {
+					break;
+				} else {
+					$count++;
+				}
+
+				$is_unread_query = mysqli_query($this->con, "SELECT opened FROM messages WHERE user_to='$userLoggedIn' AND user_from='$username' ORDER BY id DESC");
+				$row = mysqli_fetch_array($is_unread_query);
+				$style = ($row['opened'] == 'no') ? "background-color: #bdc3c7" : "";
+
+			$user_found_obj = new User($this->con, $username);
+			$latest_message_details = $this->getLatestMessage($userLoggedIn, $username);
+
+			$dots = (strlen($latest_message_details[1]) >= 12) ? "..." : "";
+			$split = str_split($latest_message_details[1], 12);
+			$split = $split[0] . $dots;
+
+			$return_string .= "<a href='messages.php?u=$username'> <div class='user_found_messages'>
+												<img src='" . $user_found_obj->getProfilePic() . "' style='height: 25px; border-radius: 50px; margin-right:5px; float:left'><strong>"
+												. $user_found_obj->getFirstAndLastName() . "</strong><br>
+												<span class='timestamp_smaller' id='info'>" . $latest_message_details[2] . "</span>
+												<p id='info' style='margin:0'><em>" . $latest_message_details[0] . "</em>" . $split . "</p></div></a>";
+		}
+		return $return_string;
+	}
+
 }// End  Class
 
 
