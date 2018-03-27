f85b0dd8ec7f3192aec3e678d2c1dcae416d17a8
Cathleen Wright <cathleenmwright@gmail.com>
2018-03-27 12:41:33 -0400

    13.148 search bar query

--- includes/handlers/ajax_search.php
+++ includes/handlers/ajax_search.php
@@ -8,4 +8,17 @@ $userLoggedIn = $_POST['userLoggedIn'];
 
 $names = explode(" ", $query); //query split into array at ' '
 
+if(strpos($query, '_') !== false) {
+  //If query contains an _ , assume user is searching for username (first_last)
+  $usersReturnedQuery = mysqli_query($con, "SELECT * FROM users WHERE username LIKE '$query%' AND user_closed='no' LIMIT 8");
+} else if (count($names) == 2) {
+  //If there are two words, assume they are first and last names respectively
+  $usersReturnedQuery = mysqli_query($con, "SELECT * FROM users WHERE (first_name LIKE '$names[0]%' AND last_name LIKE '$names[1]%') AND user_closed='no' LIMIT 8");
+} else {
+  //If query has one word only, search all first names and last names
+  $usersReturnedQuery = mysqli_query($con, "SELECT * FROM users WHERE (first_name LIKE '$names[0]%' OR last_name LIKE '$names[0]%') AND user_closed='no' LIMIT 8");
+}
+
+
+
  ?>
