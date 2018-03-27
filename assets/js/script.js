ed5720b341c4c9e58b627128235ccbddac97e6fe
Cathleen Wright <cathleenmwright@gmail.com>
2018-03-27 12:16:18 -0400

    13.146 Get live search function

--- assets/js/script.js
+++ assets/js/script.js
@@ -72,3 +72,21 @@ function getDropdownData(user, type) {
     $(".dropdown_data_window").css({"padding":"0px", "height": "0px", "border": "none", "box-shadow": "0 0 0 0"});
   }
 };
+
+//Live Search
+function getLiveSearchUsers(value, user) {
+  $.post("includes/handlers/ajax_search.php", {query: value, userLoggedIn: user}, function(data) {
+    if($(".search_results_footer_empty")[0]) {
+      $(".search_results_footer_empty")[0].toggleClass('search_results_footer');
+      $(".search_results_footer_empty")[0].toggleClass('search_results_footer_empty');
+    }
+
+    $('.search_results').html(data);
+    $('.search_results_footer').html("<a href='search.php?q=" + value + "'>See All Results</a>");
+    if(data == '') {
+      $('.search_results_footer').html('');
+      $('.search_results_footer').toggleClass('search_results_footer_empty');
+      $('.search_results_footer').toggleClass('search_results_footer');
+    }
+  });
+};
