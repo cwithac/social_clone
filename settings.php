38b787c5aba4c871c4dc62f51f69fd30ea957f6a
Cathleen Wright <cathleenmwright@gmail.com>
2018-03-27 17:29:27 -0400

    15.159 Link to upload profile pic

--- settings.php
+++ settings.php
@@ -10,4 +10,6 @@ include('includes/settings_handler.php');
   <?php
     echo "<img src='" . $user['profile_pic'] . "' id='small_profile_pics'>";
    ?>
+   <br>
+   <a href="upload.php">Upload New Profile Picture</a> <br><br>
 </div>
