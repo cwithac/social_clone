a2450853ac460a018bf4f1a1186f10551e769d38
Cathleen Wright <cathleenmwright@gmail.com>
2018-03-27 12:47:57 -0400

    13.149 Ajax search return string

--- includes/handlers/ajax_search.php
+++ includes/handlers/ajax_search.php
@@ -19,6 +19,29 @@ if(strpos($query, '_') !== false) {
   $usersReturnedQuery = mysqli_query($con, "SELECT * FROM users WHERE (first_name LIKE '$names[0]%' OR last_name LIKE '$names[0]%') AND user_closed='no' LIMIT 8");
 }
 
+if($query != '') {
+  //if query is not blank
+  while($row = mysqli_fetch_array($usersReturnedQuery)) {
+    $user = new User($con, $userLoggedIn);
+    if($row['username'] != $userLoggedIn) {
+      //get mutual friends
+      $mutual_friends = $user->getMutualFriends($row['username']) . ' friends in common.';
+    } else {
+      $mutual_friends = '';
+    }
 
+    echo "<div class='resultsDisplay'>
+            <a href='" . $row['username'] . "' style='color: #000>
+              <div class='liveSearchProfilePic'>
+                <img src='" . $row['profile_pic'] . "'>
+              </div>
+              <div class='liveSearchText'>
+                " . $row['first_name'] . ' ' . $row['last_name'] . "
+                <p id='grey'>" . $mutual_friends . " </p>
+              </div>
+            </a>'
+          </div>"
+  }
+}
 
  ?>
