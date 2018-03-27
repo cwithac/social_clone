66c0a0614aba24f830b2b98bc5638ca4003a22d7
Cathleen Wright <cathleenmwright@gmail.com>
2018-03-27 17:35:39 -0400

    15.160 User details update page templating

--- settings.php
+++ settings.php
@@ -12,4 +12,24 @@ include('includes/settings_handler.php');
    ?>
    <br>
    <a href="upload.php">Upload New Profile Picture</a> <br><br>
+
+   <h6>Update Details</h6>
+   <form action="settings.php" method="POST">
+     First Name: <input type="text" name="first_name" value="<?php echo $user['first_name']; ?>"><br>
+     Last Name: <input type="text" name="last_name" value="<?php echo $user['last_name']; ?>"><br>
+     E-Mail: <input type="text" name="email" value="<?php echo $user['email']; ?>"><br>
+   </form>
+
+   <h6>Change Password</h6>
+   <form action="settings.php" method="POST">
+     Old Password: <input type="password" name="old_password"><br>
+     New Password: <input type="password" name="new_password_1"><br>
+     Confirm New Password: <input type="password" name="new_password_2"><br>
+   </form>
+
+   <h6>Close Account</h6>
+   <form action="settings.php">
+     <input type="submit" name="close_account" id="close_account" value="Close Account">
+   </form>
+
 </div>
