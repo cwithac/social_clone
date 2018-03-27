bc6c867e25f544cd3f954b33294c4edaeb03edd2
Cathleen Wright <cathleenmwright@gmail.com>
2018-03-27 19:01:42 -0400

    15.166 Account settings and close account styling

--- settings.php
+++ settings.php
@@ -7,11 +7,12 @@ include('includes/form_handlers/settings_handler.php');
 
 <div class="main_column column">
   <h4>Account Settings</h4>
+  <hr>
   <?php
     echo "<img src='" . $user['profile_pic'] . "' id='small_profile_pics'>";
    ?>
    <br>
-   <a href="upload.php">Upload New Profile Picture</a> <br><br>
+   <a href="upload.php">Upload New Profile Picture</a> <br><br><br><hr>
 
    <?php
 
@@ -23,26 +24,24 @@ include('includes/form_handlers/settings_handler.php');
    $email = $row['email'];
 
     ?>
-
-   <h6>Update Details</h6>
+   <h6 class='settings_header'>Update Details</h6>
    <form action="settings.php" method="POST">
-     First Name: <input type="text" name="first_name" value="<?php echo $first_name; ?>"><br>
-     Last Name: <input type="text" name="last_name" value="<?php echo $last_name; ?>"><br>
-     E-Mail: <input type="text" name="email" value="<?php echo $email; ?>"><br>
+     First Name: <input type="text" name="first_name" value="<?php echo $first_name; ?>" class="settings_input"><br>
+     Last Name: <input type="text" name="last_name" value="<?php echo $last_name; ?>" class="settings_input"><br>
+     E-Mail: <input type="text" name="email" value="<?php echo $email; ?>" class="settings_input"><br>
      <?php echo $message; ?>
-     <input type="submit" name="update_details" id="save_details" value="Update Details">
+     <input type="submit" name="update_details" class="save_details warning" value="Update Details">
    </form>
-
-   <h6>Change Password</h6>
+<hr>
+   <h6 class='settings_header'>Change Password</h6>
    <form action="settings.php" method="POST">
-     Old Password: <input type="password" name="old_password"><br>
-     New Password: <input type="password" name="new_password_1"><br>
-     Confirm New Password: <input type="password" name="new_password_2"><br>
+     Old Password: <input type="password" name="old_password" class="settings_input"><br>
+     New Password: <input type="password" name="new_password_1" class="settings_input"><br>
+     Confirm New Password: <input type="password" name="new_password_2" class="settings_input"><br>
      <?php echo $password_message; ?>
-     <input type="submit" name="update_password" id="save_details" value="Change Password">
+     <input type="submit" name="update_password" class="save_details warning" value="Change Password">
    </form>
-
-   <h6>Close Account</h6>
+<hr>
    <form action="settings.php" method="POST">
      <input class='danger' type="submit" name="close_account" id="close_account" value="Close Account">
    </form>
